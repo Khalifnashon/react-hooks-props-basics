63e24f5e4c63c9cfebaecf67c223e6d43ed090aa
+ <+>
2023-01-11 08:19:01 +0300

    parts of assignment understood

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -2,9 +2,18 @@ import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
+
+// we create a prop called articleText that we assign a value of "Dear Reader: 
+// Bjarne Stroustrup has the perfect lecture oration." This value is accessible from 
+// within the BlogContent component as props.articleText!
+
+
+
 function BlogPost() {
   return (
     <div id="blog-post">
+      {/* BlogContent is being returned from BlogPost */}
+      {/* Therefore, BlogContent is a child of BlogPost */}
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
